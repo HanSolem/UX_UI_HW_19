1428b5eb8bb64e1ae73d1006522617e2e722b9a9
HanSolem <brian.r.solem@gmail.com>
2022-11-05 22:04:16 -0500

    Updates

--- js/index.js
+++ js/index.js
@@ -1,15 +1,15 @@
 
-console.log("Your index file is working correctly!");
-const open = document.getElementById('open');
-const modal_container = document.getElementById('modal_container');
-const close = document.getElementById('close');
+// console.log("Your index file is working correctly!");
+// const open = document.getElementById('open');
+// const modal_container = document.getElementById('modal_container');
+// const close = document.getElementById('close');
 
-open.addEventListener("click", () => {
-    modal_container.classList.add('show')
-});
-close.addEventListener("click", () => {
-    modal_container.classList.remove('show')
-});
+// open.addEventListener("click", () => {
+//     modal_container.classList.add('show')
+// });
+// close.addEventListener("click", () => {
+//     modal_container.classList.remove('show')
+// });
 
 $("button").hover(function(){
     $(this).css("background-color", "#3c4048");
@@ -17,3 +17,4 @@ $("button").hover(function(){
     $(this).css("background-color", "#65c8d6");
   });
 
+  
